4b346ec76577c049131be7499a2836829f116ca7
MT91-dev <tariqm1991@gmail.com>
2022-07-20 19:48:49 +0000

    Add get/post ajax requests for loading tweets and timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,44 +6,6 @@
 
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
-
   const createTweetElement = function (tweet) {
 
     const $tweet = $(`
@@ -61,7 +23,7 @@ $(document).ready(function () {
 
       <footer>
         <div>
-          <p class="elapsedTime">${tweet.created_at}</p>
+          <p class="elapsedTime">${timeago.format(tweet.created_at)}</p>
         </div>
         <div class="icons">
             <i class="fa-solid fa-flag"></i>
@@ -85,6 +47,31 @@ $(document).ready(function () {
     }
   }
 
-  renderTweets(data);
+  const $tweetForm = $('#tweet-form');
 
+  $tweetForm.submit(function(event) {
+    event.preventDefault(); //will not submit the old fashioned way, we want to submit an ajax request instead
+
+
+    $.ajax({
+      type: "POST",
+      url: `/tweets`,
+      data: $(this).serialize(),
+    }).then(function() {
+      loadTweets()
+    })
+    // console.log($(this).serialize());
+  })
+
+  const loadTweets = function() {
+    $(".tweet-container").empty();
+
+    $.ajax({
+      type: "GET",
+      url: `/tweets`,
+      success: function(data) {
+        renderTweets(data);
+      },
+    });
+  }
 });
