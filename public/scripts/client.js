1cd997278909b5c53ec30b806386cbdccddc9106
MT91-dev <tariqm1991@gmail.com>
2022-07-21 20:53:49 +0000

    Clean up HTML/JS/CSS and add breakpoint for mobile/desktop view

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,16 @@
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
+//Starting the client side javascript with the document.ready so that DOM is loaded and ready
 $(document).ready(function () {
 
+  //function that creates the entire HTML for a new tweet and returns the HTML
   const createTweetElement = function (tweet) {
 
+    //sanitizing users input so that tags and other potentially malicious tweets can be handled
     const $userInput = $("<p>").text(tweet.content.text)
 
+    //storing the tweet html in a variable and replacing the correct information like avatar, name, handle and time created via accessing the tweet from database
     const $tweet = $(`
 
       <article class="tweetFeed">
@@ -38,6 +42,7 @@ $(document).ready(function () {
     return $tweet;
   }
 
+  //function that looks at each tweet inside the tweet-container which is our database, creates each tweet element, and prepends to the container
   const renderTweets = function (tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
@@ -45,16 +50,19 @@ $(document).ready(function () {
 
     for (const tweet of tweets) {
       let $currentTweet = createTweetElement(tweet);
-      $(".tweet-container").prepend($currentTweet); //prepend instead of append shows ascending order of tweets based on date
+      $(".tweet-container").prepend($currentTweet); //prepend instead of append shows ascending order of tweets based on date, new at top and old at bottom
     }
   }
 
+  //accessing the tweet form via the classs and assigning into jQuery variable
   const $tweetForm = $('.tweet-form');
 
+  //Event handler for when user submits a tweet in the form
   $tweetForm.submit(function (event) {
     event.preventDefault(); //will not submit the old fashioned way, we want to submit an ajax request instead
     $(".error-message").hide(); //as a default, hides the error message window unless required below in conditional
 
+    //validation to check if the submission is empty or too long, with appropriate error messages displayed
     if ($("#tweet-text").val().length === 0) {
       $(".error-message").show()
       $(".error-message").text("The tweet you have entered is empty, please try again!");
@@ -65,16 +73,18 @@ $(document).ready(function () {
       return;
     }
 
+    //request to the server with a valid tweet submission, that serializes the data as query string
     $.ajax({
       type: "POST",
       url: `/tweets`,
       data: $(this).serialize(),
     }).then(function () {
-      event.target[0].value = "";
-      loadTweets();
+      event.target[0].value = ""; //resets the form after submission to be blank
+      loadTweets(); // running the loadTweets function each time a user submits a tweet
     })
   })
 
+  //function that loads the tweets by first emptying the tweet-container, sending the get ajax request fromt he server, and on success, running the renderTweets function on the entire dataset which will be all the tweets
   const loadTweets = function () {
     $(".tweet-container").empty();
 
@@ -87,5 +97,6 @@ $(document).ready(function () {
     });
   }
 
+  //running the loadTweets function once prior to any requests are made by the user to ensure stored tweets from before are already loaded on the page when first visited
   loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,24 @@
+//Starting the with the document.ready so that the DOM can read JS code once it is ready
 $(document).ready(function () {
-  // --- our code goes here ---
+
+  //storing the element by class tweet-text into tweetText variable
   const tweetText = document.getElementById("tweet-text");
 
+  //adding an event handler input on tweetText element with jQuery
   $(tweetText).on('input', function() {
+    //listening for the characters entered, getting their length, subtracting from the limit and traversing up the dom tree to find the element for counter via the class
     let charEntered = ($(this).val().length);
     let charRemaining = 140 - charEntered;
     let targetCounter = ($(this).parent().children().children('.counter'));
     
+    //minor validation that adds a new class "red" to the counter so that CSS can be applied via the red class
     if (charRemaining < 0) {
       targetCounter.addClass('red');
     } else {
       targetCounter.removeClass('red');
     }
 
+    //replacing the text that is shown at the counter element with the charRemaining variable computed from above
     $(targetCounter).text(charRemaining);
     
   });
